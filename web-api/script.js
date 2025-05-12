cef2e8e9db40d6a1f8e47e4494380e694215620f
Leonardo Tavares <leo.tav.12345@gmail.com>
2025-05-12 20:07:42 -0300

    feat: web animations

--- web-api/script.js
+++ web-api/script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const squareAnimation = element.animate(
     [
-      { transform: "translateX(0)" },
-      { backgrundColor: "blue", offset: 0.8 },
+      { transform: "translateX(0)", easing: "ease-in" },
+      { backgroundColor: "blue", offset: 0.8 }, // offset é a porcentagem da animacao que o keyframe vai ser executado
       {
         transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
         backgrundColor: "red",
@@ -18,10 +18,43 @@ document.addEventListener("DOMContentLoaded", () => {
       iterations: Infinity,
       direction: "alternate",
       fill: "forwards",
+      iterationComposite: "accumulate",
       composite: "add",
       timeline: document.timeline,
     }
   );
+
+  const buttons = document.querySelectorAll(".button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.classList.contains("play")) {
+        squareAnimation.play();
+      }
+      if (button.classList.contains("pause")) {
+        squareAnimation.pause();
+      }
+      if (button.classList.contains("cancel")) {
+        squareAnimation.cancel();
+      }
+      if (button.classList.contains("reverse")) {
+        squareAnimation.reverse();
+      }
+      if (button.classList.contains("finish")) {
+        squareAnimation.finish();
+      }
+    });
+
+    const playbackRateInput = document.getElementById("playbackRateInput");
+    const playbackRateInputValue = document.getElementById(
+      "playbackRateInputValue"
+    );
+
+    playbackRateInput.addEventListener("input", (e) => {
+      squareAnimation.updatePlaybackRate(e.target.value);
+      playbackRateInputValue.value = e.target.value;
+    });
+  });
 
   // com new Animation e KeyframeEffect precisa chamar o metodo play para iniciar
 
